e61e808380596c60f146fa533026318ea3502357
ColleenCronin45 <colleencronin2.0@gmail.com>
2021-11-28 18:46:24 -0500

    Added some comments

--- Spine_RunFile.R
+++ Spine_RunFile.R
@@ -1,9 +1,12 @@
 library(foreign)
 
+#source will run the 2 code files
 source(here("GBIFcode.R"))
 source(here("ArctosCode.R"))
 
+#combining GBIF and Arctos datasets
 BatRodentData = bind_rows(data2, dataset2)
 
+#exporting data as a .dbf
 write.dbf(as.data.frame(BatRodentData), here("BatRodentData.dbf"))
 
